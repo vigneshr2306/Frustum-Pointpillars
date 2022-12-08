b3c501b398c0de09771723e896845fe9644a7e32
vigneshr2306 <vigneshr2306@github.com>
2022-12-07 23:51:43 -0500

    final setup

--- second/core/cc/nms/nms_cpu.h
+++ second/core/cc/nms/nms_cpu.h
@@ -1,23 +1,26 @@
 #ifndef NMS_CPU_H
 #define NMS_CPU_H
-#include <boost/geometry/geometries/geometries.hpp>
 #include <pybind11/pybind11.h>
+
+#include <boost/geometry/geometries/geometries.hpp>
+#include <iostream>
 // must include pybind11/stl.h if using containers in STL in arguments.
-#include <pybind11/stl.h>
 #include <pybind11/numpy.h>
-#include <vector>
+#include <pybind11/stl.h>
+
 #include <algorithm>
 #include <boost/geometry.hpp>
-template<typename DType, typename ShapeContainer>
-inline py::array_t<DType> constant(ShapeContainer shape, DType value){
+#include <vector>
+template <typename DType, typename ShapeContainer>
+inline py::array_t<DType> constant(ShapeContainer shape, DType value) {
     // create ROWMAJOR array.
     py::array_t<DType> array(shape);
     std::fill(array.mutable_data(), array.mutable_data() + array.size(), value);
     return array;
 }
 
-template<typename DType>
-inline py::array_t<DType> zeros(std::vector<long int> shape){
+template <typename DType>
+inline py::array_t<DType> zeros(std::vector<long int> shape) {
     return constant<DType, std::vector<long int>>(shape, 0);
 }
 
@@ -26,8 +29,7 @@ std::vector<int> non_max_suppression_cpu(
     py::array_t<DType> boxes,
     py::array_t<int> order,
     DType thresh,
-    DType eps=0)
-{
+    DType eps = 0) {
     auto ndets = boxes.shape(0);
     auto boxes_r = boxes.template unchecked<2>();
     auto order_r = order.template unchecked<1>();
@@ -36,32 +38,32 @@ std::vector<int> non_max_suppression_cpu(
     auto area = zeros<DType>({ndets});
     auto area_rw = area.template mutable_unchecked<1>();
     // get areas
-    for(int i = 0; i < ndets; ++i){
+    for (int i = 0; i < ndets; ++i) {
         area_rw(i) = (boxes_r(i, 2) - boxes_r(i, 0) + eps) * (boxes_r(i, 3) - boxes_r(i, 1) + eps);
     }
     std::vector<int> keep;
     int i, j;
     DType xx1, xx2, w, h, inter, ovr;
-    for(int _i = 0; _i < ndets; ++_i){
+    for (int _i = 0; _i < ndets; ++_i) {
         i = order_r(_i);
-        if(suppressed_rw(i) == 1)
+        if (suppressed_rw(i) == 1)
             continue;
         keep.push_back(i);
-        for(int _j = _i + 1; _j < ndets; ++_j){
+        for (int _j = _i + 1; _j < ndets; ++_j) {
             j = order_r(_j);
-            if(suppressed_rw(j) == 1)
+            if (suppressed_rw(j) == 1)
                 continue;
             xx2 = std::min(boxes_r(i, 2), boxes_r(j, 2));
             xx1 = std::max(boxes_r(i, 0), boxes_r(j, 0));
             w = xx2 - xx1 + eps;
-            if (w > 0){
+            if (w > 0) {
                 xx2 = std::min(boxes_r(i, 3), boxes_r(j, 3));
                 xx1 = std::max(boxes_r(i, 1), boxes_r(j, 1));
                 h = xx2 - xx1 + eps;
-                if (h > 0){
+                if (h > 0) {
                     inter = w * h;
                     ovr = inter / (area_rw(i) + area_rw(j) - inter);
-                    if(ovr >= thresh)
+                    if (ovr >= thresh)
                         suppressed_rw(j) = 1;
                 }
             }
@@ -75,8 +77,7 @@ std::vector<int> rotate_non_max_suppression_cpu(
     py::array_t<DType> box_corners,
     py::array_t<int> order,
     py::array_t<DType> standup_iou,
-    DType thresh)
-{
+    DType thresh) {
     auto ndets = box_corners.shape(0);
     auto box_corners_r = box_corners.template unchecked<3>();
     auto order_r = order.template unchecked<1>();
@@ -86,21 +87,21 @@ std::vector<int> rotate_non_max_suppression_cpu(
     std::vector<int> keep;
     int i, j;
 
-    //namespace bg = boost::geometry;
-    typedef boost::geometry::model::point<DType, 2, bg::cs::cartesian> point_t;
+    // namespace bg = boost::geometry;
+    typedef boost::geometry::model::point<DType, 2, boost::geometry::cs::cartesian> point_t;
     typedef boost::geometry::model::polygon<point_t> polygon_t;
     polygon_t poly, qpoly;
     std::vector<polygon_t> poly_inter, poly_union;
     DType inter_area, union_area, overlap;
 
-    for(int _i = 0; _i < ndets; ++_i){
+    for (int _i = 0; _i < ndets; ++_i) {
         i = order_r(_i);
-        if(suppressed_rw(i) == 1)
+        if (suppressed_rw(i) == 1)
             continue;
         keep.push_back(i);
-        for(int _j = _i + 1; _j < ndets; ++_j){
+        for (int _j = _i + 1; _j < ndets; ++_j) {
             j = order_r(_j);
-            if(suppressed_rw(j) == 1)
+            if (suppressed_rw(j) == 1)
                 continue;
             if (standup_iou_r(i, j) <= 0.0)
                 continue;
@@ -119,11 +120,11 @@ std::vector<int> rotate_non_max_suppression_cpu(
                 boost::geometry::intersection(poly, qpoly, poly_inter);
             } catch (const std::exception& e) {
                 std::cout << "box i corners:" << std::endl;
-                for(int k = 0; k < 4; ++k){
+                for (int k = 0; k < 4; ++k) {
                     std::cout << box_corners_r(i, k, 0) << " " << box_corners_r(i, k, 1) << std::endl;
                 }
-                std::cout << "box j corners:" <<  std::endl;
-                for(int k = 0; k < 4; ++k){
+                std::cout << "box j corners:" << std::endl;
+                for (int k = 0; k < 4; ++k) {
                     std::cout << box_corners_r(j, k, 0) << " " << box_corners_r(j, k, 1) << std::endl;
                 }
                 // throw e;
@@ -131,8 +132,7 @@ std::vector<int> rotate_non_max_suppression_cpu(
             }
             // std::cout << "post_poly" << std::endl;
             // std::cout << "post_intsec" << std::endl;
-            if (!poly_inter.empty())
-            {
+            if (!poly_inter.empty()) {
                 inter_area = boost::geometry::area(poly_inter.front());
                 // std::cout << "pre_union" << " " << inter_area << std::endl;
                 boost::geometry::union_(poly, qpoly, poly_union);
@@ -149,12 +149,12 @@ std::vector<int> rotate_non_max_suppression_cpu(
                     }
                 }*/
                 // std::cout << "post_union" << poly_union.empty() << std::endl;
-                if (!poly_union.empty()){ // ignore invalid box
-                    union_area = bg::area(poly_union.front());
+                if (!poly_union.empty()) {  // ignore invalid box
+                    union_area = boost::geometry::area(poly_union.front());
                     // std::cout << "post union area" << std::endl;
                     // std::cout << union_area << "debug" << std::endl;
                     overlap = inter_area / union_area;
-                    if(overlap >= thresh)
+                    if (overlap >= thresh)
                         suppressed_rw(j) = 1;
                     poly_union.clear();
                 }
@@ -162,7 +162,6 @@ std::vector<int> rotate_non_max_suppression_cpu(
             poly.clear();
             qpoly.clear();
             poly_inter.clear();
-
         }
     }
     return keep;
